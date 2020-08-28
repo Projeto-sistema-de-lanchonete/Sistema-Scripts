8128173297072c2b609229318d21c8acfabb7e0c
Eduarda Thayse <67771905+eduardathayse@users.noreply.github.com>
2020-08-27 21:20:03 -0300

    Atualização do bando

--- DB/bdlanchonete.sql
+++ DB/bdlanchonete.sql
@@ -3,7 +3,7 @@ create database bdlanchonete;
 use bdlanchonete;
 
 -- --------------------------------------------------------
--- Estrutura da tabela `usuarios`
+-- Estrutura das tabelas
 
 CREATE TABLE `usuarios` (
   `id` int(4) NOT NULL,
@@ -12,26 +12,51 @@ CREATE TABLE `usuarios` (
   `senha` varchar(100) NOT NULL
 );
 
+create table produtos(
+cod_produto int(15) not null,
+ean_produto int(30),
+nome_produto varchar(100) not null,
+categoria_produto varchar(100) not null,
+descricão_produto varchar(255),
+pre_venda_produto decimal(4,2),
+pre_custo_produto decimal(4,2),
+estoque varchar(5)
+);
+
 -- --------------------------------------------------------
--- chave primaria para tabela `usuarios`
+-- chave primaria para as tabelas
 
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
+ALTER TABLE produtos
+  ADD PRIMARY KEY (cod_produto);
+  
 -- --------------------------------------------------------
--- AUTO_INCREMENT de tabela `usuarios`
+-- AUTO_INCREMENT das tabelas
 
 ALTER TABLE `usuarios`
   MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000;
 COMMIT;
 
+ALTER TABLE produtos
+  MODIFY cod_produto int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1245678;
+COMMIT;
+
 -- --------------------------------------------------------
--- Inserindo dados da tabela `usuarios`
+-- Inserindo dados nas tabelas
 
 INSERT INTO `usuarios` (nome, cpf, senha) VALUES
 ('itamar', '08919673462', '1234'),
 ('eduardathayse', '06576933401', '4321');
 
-select * from usuarios;  -- auto increment
+INSERT INTO  produtos (ean_produto, nome_produto, categoria_produto, descricão_produto, pre_venda_produto, pre_custo_produto, estoque) VALUES
+('8711253001205', 'Hamburgue', 'Lanches', 'Hamburgue carne de sol desfida', '16,00', '16,00', '5'),
+('901234123457', 'Coxinha', 'Salgados', 'Coxinha de Frango', '8,00', '10,00', '10');
+
+
+select * from usuarios;  
+
+select * from produtos;
 
 -- deletar linha da tabela  ->>  delete from usuarios where id = 1002;
